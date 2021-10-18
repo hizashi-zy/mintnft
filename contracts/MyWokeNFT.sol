83e018bafcc1e3c6de9dd572b002db555c8146ae
hizashi <zytwp@hotmail.com>
2021-10-18 21:28:17 +0800

    wew done

--- contracts/MyWokeNFT.sol
+++ contracts/MyWokeNFT.sol
@@ -2,9 +2,12 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
+
+import {Base64} from "./libraries/Base64.sol";
 
 contract MyWokeNFT is ERC721URIStorage {
 
@@ -12,15 +15,109 @@ contract MyWokeNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
+    // So, we make a baseSvg variable here that all our NFTs can use.
+    //svg is split at the part where it asks for the background colour
+    string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+    string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    // I create three arrays, each with their own theme of random words.
+    // Pick some random funny words, names of anime characters, foods you like, whatever! 
+    string[] firstWords = ["Epic", "Woke", "Enlightened", "Dope", "Salty", "Cool", "Sexy", "Legendary", "Awesome", "Mysterious", "Haunting", "WHAT", "Sad", "Angry", "Powerful", "Delicious"];
+    string[] secondWords = ["Zoro", "Luffy", "BoaHancock", "Sanji", "Brook", "Chopper", "Nami", "WHO", "Sunny", "Franky", "Jimbe", "Robin", "Yamato", "Dragon", "Ace", "Sabo", "Garp"];
+    string[] thirdWords = ["Eye", "Punch", "Look", "Hands", "Wings", "Legs", "Head", "HUH", "Bottle", "Sword", "Violin", "Feet", "Lips", "Eyes", "Nose", "Sake", "Fruit", "Haki", "Hat", "Kick", "Laptop", "DenDenMushi"];
+    
+    //declare a bunch of colours
+    string[] colours = ["orange", "green", "black", "blueviolet", "lime", "darkkhaki"];
+
+    event NewWokeNFTMinted(address sender, uint256 tokenId);
+
     constructor() ERC721 ("TheWokeNFT", "WOKE") {
         console.log("This is my NFT contract");
     }
 
+    // I create a function to randomly pick a word from each array.
+    function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+        // I seed the random generator. More on this in the lesson. 
+        uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+        // Squash the # between 0 and the length of the array to avoid going out of bounds.
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
+
+
+    function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
+
+    function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
+
+    //pick a random colour
+    function pickRandomColour(uint tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("COLOUR", Strings.toString(tokenId))));
+        rand = rand % colours.length;
+        return colours[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
+    }
+
     function makeAnNFT() public {
         uint256 newItemId = _tokenIds.current();
+
+        // We go and randomly grab one word from each of the three arrays.
+        string memory first = pickRandomFirstWord(newItemId);
+        string memory second = pickRandomSecondWord(newItemId);
+        string memory third = pickRandomThirdWord(newItemId);
+        string memory combinedWord = string(abi.encodePacked(first, second, third));
+
+        //random colour here
+        string memory randomColour = pickRandomColour(newItemId);
+        string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColour, svgPartTwo, combinedWord, "</text></svg>"));
+
+
+        //string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
+
+        // Get all the JSON metadata in place and base64 encode it.
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        // We set the title of our NFT as the generated word.
+                        combinedWord,
+                        '", "description": "A highly acclaimed collection of Wokes.", "image": "data:image/svg+xml;base64,',
+                        // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                        Base64.encode(bytes(finalSvg)),
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        //just like before we prepend data:application/json;base64, to our data.
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        console.log("\n--------------------");
+        console.log(finalTokenUri);
+        console.log("--------------------\n");
+
         _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/RHPQ");
-        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+        _setTokenURI(newItemId, finalTokenUri);
+
         _tokenIds.increment();
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+        
+        emit NewWokeNFTMinted(msg.sender, newItemId);
     }
 }
